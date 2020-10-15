ba06ea7dfffc2e3d0fac3e0b75ad2ef322de50cf
Simone <simone.bianchin99@gmail.com>
2020-10-15 23:34:09 +0200

    Init react scaffolding

---
+++ src/componenets/atoms/hello-world/HelloWorld.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import "./HelloWorld.style.scss"
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <span>Hello JEToP</span>
+      <br />
+      <button>Cliccami</button>
+    </div>
+  )
+}
+export default HelloWorld
