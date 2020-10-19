012e48df9ffe86b01712f2333a3ae78e2065741c
TheGrinch00 <antonio.ascone997@gmail.com>
2020-10-19 21:54:46 +0200

    Add basic routing system and protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,50 @@
 import React from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom"
+
 import "./App.css"
-import HelloWorld from "./componenets/atoms/hello-world/HelloWorld"
+
+import LoginPage from "./componenets/loginPage/loginPage"
+import CredentialsPage from "./componenets/protectedArea/credentialsPage"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <HelloWorld />
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/login">
+          <LoginPage />
+        </Route>
+        <Route
+          path="/services-credentials"
+          render={() =>
+            fakeAuth.isAuthenticated ? (
+              <CredentialsPage />
+            ) : (
+              <Redirect to={{ pathname: "/login" }} />
+            )
+          }
+        />
+        <Route
+          path="*"
+          render={() =>
+            fakeAuth.isAuthenticated ? (
+              <Redirect to={{ pathname: "/services-credentials" }} />
+            ) : (
+              <Redirect to={{ pathname: "/login" }} />
+            )
+          }
+        />
+      </Switch>
+    </Router>
   )
 }
 
 export default App
+
+const fakeAuth = {
+  isAuthenticated: false,
+}
