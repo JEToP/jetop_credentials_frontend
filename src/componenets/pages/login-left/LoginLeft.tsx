45d01cffc0f5f1a472f933d49a3e80bf0ca922b8
Simone <simone.bianchin99@gmail.com>
2020-10-21 22:23:58 +0200

    Fix code cleanness

---
+++ src/componenets/pages/login-left/LoginLeft.tsx
@@ -0,0 +1,31 @@
+import Button from "../../atoms/button/Button"
+import * as React from "react"
+import "./LoginLeft.style.scss"
+
+const LoginLeft = () => {
+  return (
+    <div className="login-left">
+      <img src="/assets/jetop_logo.png" className="login-logo" />
+      <div className="login-content">
+        <div className="login-header">
+          <span className="login-title">Login.</span>
+          <br />
+          <span className="login-subtitle">
+            Effettua il login con il tuo account JEToP
+          </span>
+        </div>
+      </div>
+      <div className="login-interactive">
+        <Button label="Google Login" />
+        <span className="login-footer">
+          Non sei di JEToP?
+          <a href="https://jetop.com" className="login-url">
+            &nbsp;Candidati!
+          </a>
+        </span>
+      </div>
+      <p className="login-credits">Built with &#128151; by JEToP's ITs</p>
+    </div>
+  )
+}
+export default LoginLeft
