c31bc2918cd0a86183802f74c325ac36260ffd95
Simone <simone.bianchin99@gmail.com>
2020-10-23 22:05:26 +0200

    Wip google-sign-in

--- src/components/templates/login/LoginScreen.style.tsx
+++ src/components/templates/login/LoginScreen.style.tsx
@@ -2,22 +2,31 @@ import React, { useEffect } from "react"
 import "./LoginScreen.scss"
 import LoginLeft from "../../pages/login-left/LoginLeft"
 import LoginRight from "../../pages/login-right/LoginRight"
+import { GoogleLoginResponse, useGoogleLogin } from "react-google-login"
 
 declare var google: any
 
+const CLIENT_ID =
+  "232797005510-t37q65unc636bupg35ta2ofl12mqllq2.apps.googleusercontent.com"
+
 const LoginScreen = () => {
+  const handleCredentialResponse = (tokenId: string) => console.log(tokenId)
+  const { signIn, loaded } = useGoogleLogin({
+    clientId: CLIENT_ID,
+    onSuccess: (response: any) => handleCredentialResponse(response.tokenId),
+    onFailure: (e) => console.error(e),
+  })
   useEffect(() => {
-    const handleCredentialResponse = (e: any) => {}
     google.accounts.id.initialize({
-      client_id:
-        "869086414529-35s2e6odscj5ohkcu29ved8rbkklnieu.apps.googleusercontent.com",
-      callback: handleCredentialResponse,
+      client_id: CLIENT_ID,
+      callback: (e: any) => handleCredentialResponse(e.credential),
     })
     google.accounts.id.prompt()
   }, [])
+
   return (
     <div className="login-root">
-      <LoginLeft />
+      <LoginLeft googleLoginOnClick={signIn} />
       <LoginRight />
     </div>
   )
