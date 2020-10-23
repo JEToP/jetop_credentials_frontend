d485bc6dc4999609ba0e1897220e82f9a5441729
Simone <simone.bianchin99@gmail.com>
2020-10-23 20:21:45 +0200

    Wip google-one-tap

--- src/components/templates/login/LoginScreen.style.tsx
+++ src/components/templates/login/LoginScreen.style.tsx
@@ -1,9 +1,20 @@
-import * as React from "react"
+import React, { useEffect } from "react"
 import "./LoginScreen.scss"
 import LoginLeft from "../../pages/login-left/LoginLeft"
 import LoginRight from "../../pages/login-right/LoginRight"
 
+declare var google: any
+
 const LoginScreen = () => {
+  useEffect(() => {
+    const handleCredentialResponse = (e: any) => {}
+    google.accounts.id.initialize({
+      client_id:
+        "869086414529-35s2e6odscj5ohkcu29ved8rbkklnieu.apps.googleusercontent.com",
+      callback: handleCredentialResponse,
+    })
+    google.accounts.id.prompt()
+  }, [])
   return (
     <div className="login-root">
       <LoginLeft />
