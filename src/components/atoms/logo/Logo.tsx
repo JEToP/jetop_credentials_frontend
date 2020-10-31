797fdc2e0e91f6702444603dcafbcc25771aeac4
Michele Pulvirenti <michele00.pulvirenti@gmail.com>
2020-10-31 14:04:49 +0100

    Add menu UI

---
+++ src/components/atoms/logo/Logo.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import "./Logo.style.scss"
+
+export function Logo() {
+  return (
+    <img
+      className="logo"
+      src="https://new.jetop.com/wp-content/uploads/2017/03/jetop_resized.png"
+    />
+  )
+}
