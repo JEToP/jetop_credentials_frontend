d780c0ff297bd007ecb08c77e5a153d0f75229d6
Federico Bono <f.bono@jetop.com>
2020-10-31 23:22:30 +0100

    Fix CI

--- src/components/templates/protected-area/CredentialsPage.tsx
+++ src/components/templates/protected-area/CredentialsPage.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react"
 import { useServiceContext } from "../../../hooks/useServiceContext"
-import { ServiceListItem } from "../../atoms/service-list-item/ServiceListItem"
 import { ServiceList } from "../../molecules/service-list/ServiceList"
 import Service from "../../../models/Service"
-import { act } from "react-dom/test-utils"
 
 function CredentialsPage() {
   const [activeService, setActiveService] = useState<Service | null>(null)
