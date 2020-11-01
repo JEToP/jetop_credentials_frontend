2c38eda7a38e4313c41ed540e501b50eec559f3a
Francesca Alfonso <franky171295@tiscali.it>
2020-11-01 15:09:03 +0100

    fix problems

--- src/components/templates/protected-area/passwordGeneratorModal.tsx
+++ src/components/templates/protected-area/passwordGeneratorModal.tsx
@@ -2,53 +2,48 @@ import React, { useEffect, useState } from "react"
 // import PasswordVisualization from "./passwordVisualization"
 import { generatePassword } from "../../../libs/passwordGenerator"
 import "./passwordGenerator.scss"
-import "./password.style.scss"
+// import "./password.style.scss"
 
 //show the password casually generated with generatePassword func, user can choose lenght with slider
 const PasswGenerator = (props: any) => {
-  const [passwLength, setPasswLength] = useState(8)
-  const [isUpperCase, setUpperCase] = useState(false)
-  const [isNumber, setNumber] = useState(false)
-  const [isSpecial, setSpecial] = useState(false)
-  const [isAmbiguous, setAmbiguous] = useState(false)
+  const [passwOpt, setPasswOpt] = useState({
+    passwLength: 8,
+    useUpperCase: false,
+    useNumber: false,
+    useSpecial: false,
+    avoidAmbiguous: false,
+  })
 
-  const [PasswValue, setPasswValue] = useState(
-    generatePassword(passwLength, isUpperCase, isNumber, isSpecial, isAmbiguous)
-  )
+  const [PasswValue, setPasswValue] = useState(() => {
+    return generatePassword(
+      passwOpt.passwLength,
+      passwOpt.useUpperCase,
+      passwOpt.useNumber,
+      passwOpt.useSpecial,
+      passwOpt.avoidAmbiguous
+    )
+  })
 
   const { closeModal } = props
 
-  function handlepasswLength(e: any) {
-    setPasswLength(parseInt(e.target.PasswValue))
-  }
-
-  function handleUpperCase(e: any) {
-    setUpperCase(e.target.checked)
-  }
-
-  function handleNumber(e: any) {
-    setNumber(e.target.checked)
-  }
-
-  function handleSpecial(e: any) {
-    setSpecial(e.target.checked)
-  }
-
-  function handleAmbiguous(e: any) {
-    setAmbiguous(e.target.checked)
+  const optChangeHandle = (e: any) => {
+    setPasswOpt((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
   }
 
   useEffect(() => {
     setPasswValue(
       generatePassword(
-        passwLength,
-        isUpperCase,
-        isNumber,
-        isSpecial,
-        isAmbiguous
+        passwOpt.passwLength,
+        passwOpt.useUpperCase,
+        passwOpt.useNumber,
+        passwOpt.useSpecial,
+        passwOpt.avoidAmbiguous
       )
     )
-  }, [passwLength, isUpperCase, isNumber, isSpecial, isAmbiguous])
+  }, [passwOpt])
 
   return (
     <div className="passwGeneratorContainer">
@@ -67,24 +62,29 @@ const PasswGenerator = (props: any) => {
               type="range"
               min="6"
               max="40"
-              value={passwLength}
-              onChange={handlepasswLength}
+              id="passwLength"
+              value={passwOpt.passwLength}
+              onChange={optChangeHandle}
               className="slider"
             />
             <br />
-            <label>Numero di caratteri: {passwLength}</label>
+            <label>Numero di caratteri: {passwOpt.passwLength}</label>
             <br />
           </div>
-          <input type="checkbox" id="UpperCase" onChange={handleUpperCase} />
+          <input type="checkbox" id="useUpperCase" onChange={optChangeHandle} />
           <label>Includi lettere maiuscole</label>
           <br />
-          <input type="checkbox" id="Number" onChange={handleNumber} />
+          <input type="checkbox" id="useNumber" onChange={optChangeHandle} />
           <label>Includi caratteri numerici</label>
           <br />
-          <input type="checkbox" id="Special" onChange={handleSpecial} />
+          <input type="checkbox" id="useSpecial" onChange={optChangeHandle} />
           <label>Includi caratteri speciali</label>
           <br />
-          <input type="checkbox" id="Ambiguous" onChange={handleAmbiguous} />
+          <input
+            type="checkbox"
+            id="avoidAmbiguous"
+            onChange={optChangeHandle}
+          />
           <label>Evita caratteri ambigui</label>
           <br />
           <div className="modalFooter">
